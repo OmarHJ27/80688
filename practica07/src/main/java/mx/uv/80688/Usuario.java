99fa410ddb4cbb7a448f8c0e6657634b39bc2f34
OmarHJ27 <omar_27_hidalgo@hotmail.com>
2022-11-09 20:39:45 -0600

    creacion practica 07

---
+++ practica07/src/main/java/mx/uv/80688/Usuario.java
@@ -0,0 +1,26 @@
+package mx.uv.80688;
+
+public class Usuario {
+    String id;
+    String nombre;
+    String password;
+    
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
