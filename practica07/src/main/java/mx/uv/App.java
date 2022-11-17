5695cf3c1b82c83725f0b813b98377f542e3f85c
OmarHJ27 <omar_27_hidalgo@hotmail.com>
2022-11-17 01:02:05 -0600

    .

--- practica07/src/main/java/mx/uv/App.java
+++ practica07/src/main/java/mx/uv/App.java
@@ -15,9 +15,29 @@ public class App {
 
     public static Gson gson = new Gson();
     private static Map<String, Usuario> usuarios = new HashMap<>();
+    // private static Conexion c;
 
     public static void main(String[] args) {
+        // intentar conectar a la BD
+        // c = new Conexion();
+        // c.getConnection();
+
         port(80);
+        options("/*", (request, response) -> {
+                String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+                System.out.println(accessControlRequestHeaders);
+                if (accessControlRequestHeaders != null) {
+                    response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+                }
+                String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+                System.out.println(accessControlRequestMethod); 
+                if (accessControlRequestMethod != null) {
+                    response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+                }
+                return "OK";
+            });
+            
+        before((req, res)-> res.header("Access-Control-Allow-Origin", "*"));
         System.out.println("Hello World!");
         Usuario u1 = new Usuario("1", "Pedro", "1234");
         Usuario u2 = new Usuario("2", "Pablo", "7890");
@@ -29,14 +49,16 @@ public class App {
         // devolver un usuario
         // get("/", (req, res) -> gson.toJson(u1) ); 
         // devolver lista de usuarios
-        get("/", (req, res) -> gson.toJson(usuarios) );
+        // get("/", (req, res) -> gson.toJson(usuarios) );
+        get("/usuarios", (req, res) -> gson.toJson(DAO.dameUsuarios()) );
         // get("/", (req, res) -> u1.getNombre() );
 
         post("/", (req,res)->{
             String datosCliente = req.body();
             Usuario u = gson.fromJson(datosCliente, Usuario.class);
-            usuarios.put(u.getId(), u);
-            return "listo se ingreso el usuario "+u.getId();
+            // usuarios.put(u.getId(), u);
+            // return "listo se ingreso el usuario "+u.getId();
+            return DAO.crearUsuario(u);
         });
 
 
